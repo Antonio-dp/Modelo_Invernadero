5607c8e91e5a0adc67e1e6ce890b62835edef475
Antonio-dp <antonio.delpardo229161@potros.itson.edu.mx>
2023-05-09 00:15:57 -0700

    ea

---
+++ Modelo_Invernadero/src/main/java/com/mycompany/modelo_invernadero/Modelo_Invernadero.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.modelo_invernadero;
+
+/**
+ *
+ * @author tonyd
+ */
+public class Modelo_Invernadero {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
